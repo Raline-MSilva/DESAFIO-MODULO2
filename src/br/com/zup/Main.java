38d2de628cf80a75ec24bacd3f7a7ea7be412da8
Raline Silva <ralinesilva1@gmail.com>
2021-09-03 18:36:57 -0300

    Add o funcionarios.remove p/ deletar por CPF

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -61,6 +61,7 @@ public class Main {
                             cpfDeletado = cpfASerDeletado;
                         }
                     }
+                    funcionarios.remove(cpfDeletado);
                     break;
 
                 }
